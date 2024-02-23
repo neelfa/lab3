0bd06cbfed1f39ce2d5f2189c17edc2006aa0cc7
keisha elfa <keishaelfa16@gmail.com>
2024-02-23 22:15:20 +0800

    added ci4 tutorial code for news insert

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -6,9 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-
+use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);           
+$routes->get('news/new', [News::class, 'new']); // Add this line
+$routes->post('news', [News::class, 'create']); // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); 
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
+
